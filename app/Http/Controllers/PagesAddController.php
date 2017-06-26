648cb820808e3e5834b88bc20a5ecd12044397a7
anb05 <alexandr05@list.ru>
2017-06-26 09:39:34 +0300

    add PagesAddController

--- app/Http/Controllers/PagesAddController.php
+++ app/Http/Controllers/PagesAddController.php
@@ -8,6 +8,23 @@ class PagesAddController extends Controller
 {
     public function execute(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $input = $request->except('_token');
+
+            $validator = \Validator::make($input, [
+                'name' => 'required|max:255',
+                'alias'=> 'required|unique:pages|max:255',
+                'text' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->route('pagesAdd')->withErrors($validator)->withInput();
+            }
+
+            $file = $request->file('images');
+//            dd($file);
+        }
+
         if (view()->exists('admin.pages_add'))
         {
             $data = ['title' => 'Новая страница'];
--- resources/views/admin/content_pages_add.blade.php
+++ resources/views/admin/content_pages_add.blade.php
@@ -4,7 +4,7 @@
         <div class="form-group">
             <label for="name" class="col-xs-2 control-label">Название</label>
             <div class="col-xs-8">
-                <input type="text" id="name" value="{{ old('name') }}"
+                <input name="name" type="text" id="name" value="{{ old('name') }}"
                        class="form-control" placeholder="Введите название страницы">
             </div>
         </div>
@@ -12,7 +12,7 @@
         <div class="form-group">
             <label for="alias" class="col-xs-2 control-label">Введите псевдоним страницы</label>
             <div class="col-xs-8">
-                <input type="text" id="alias" value="{{ old('alias') }}"
+                <input name="alias" type="text" id="alias" value="{{ old('alias') }}"
                        class="form-control" placeholder="Введите псевдоним страницы">
             </div>
         </div>
@@ -20,19 +20,25 @@
         <div class="form-group">
             <label for="editor" class="col-xs-2 control-label">Текст</label>
             <div class="col-xs-8">
-                <textarea type="text" id="editor"
+                <textarea name="text" type="text" id="editor"
                           class="form-control" placeholder="Введите текст страницы">{{ old('alias') }}</textarea>
             </div>
         </div>
-    </form>
 
-    <div class="form-group">
-        <label for="images" class="col-xs-2 control-label">Изображение</label>
-        <div class="col-xs-8">
-            <input type="file" id="images" value="{{ old('alias') }}"
-                   class="filestyle" placeholder="Изображения нет">
+        <div class="form-group">
+            <label for="images" class="col-xs-2 control-label">Изображение</label>
+            <div class="col-xs-8">
+                <input name="images" type="file" id="images" value="{{ old('alias') }}"
+                       class="filestyle" placeholder="Изображения нет">
+            </div>
         </div>
-    </div>
+
+        <div class="form-group">
+            <div class="col-xs-offset-2 col-xs-10">
+                <button type="submit" class="btn btn-primary">Сохранить</button>
+            </div>
+        </div>
+    </form>
     <script>
         CKEDITOR.replace('editor');
     </script>
