fb8794540c2e4554c057f96659d86f4b4f3bd0e8
anb05 <alexandr05@list.ru>
2017-06-27 22:28:22 +0300

    The admin/portfolio section starts to be created

--- routes/web.php
+++ routes/web.php
@@ -146,6 +146,6 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function ()
 });
 
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
